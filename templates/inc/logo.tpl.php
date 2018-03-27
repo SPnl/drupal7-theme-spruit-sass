1332a8c982279aa87d28532f81a58f9da1d97639
chriseikelboom <ceikelboom@sp.nl>
2018-03-27 09:50:49 +0200

    added afdsearch, figure, adjusted badge, doemee, shame, logo

--- templates/inc/logo.tpl.php
+++ templates/inc/logo.tpl.php
@@ -1,10 +1,10 @@
 <?php if (theme_get_setting("default_logo")) :?>
-<!--[if gte IE 9]><!-->
+<!--[if gte IE 9]>
   <?php include(path_to_theme().'/logo2.svg'); ?>
-<!--<![endif]-->
+<![endif]-->
 <!--[if lte IE 8]>
   <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-<!--<![endif]-->
+<![endif]-->
 <?php else: ?>
   <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
 <?php endif; ?>
