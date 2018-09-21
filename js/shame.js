0330a305bc432ce575ccb03e3a5bf3b491a7c37a
Stijn Dekker <sdekker@sp.nl>
2018-09-21 13:11:09 +0200

    Moved shame.js node overrides to database (code per node module).

--- js/shame.js
+++ js/shame.js
@@ -1,10 +1,5 @@
 (function($) {
-  // replace h1 title with svg logo rechtvaardigheid
   $(document).ready(function(){
-    $(".page-node-60 .sidetitle").replaceWith("<img style=\"max-width: 800px;\" class=\"logo-rechtvaardigheid\" src=\"/files/rechtvaardigheid_wit.svg\">");
-    $(".page-node-60 .field-cta-subtitle .field-item").replaceWith("<div class=\"field-item\"></div>");
-    $(".page-node-61 .sidetitle").replaceWith("<img style=\"max-width: 800px;\" class=\"logo-rechtvaardigheid\" src=\"/files/rechtvaardigheid_wit.svg\">");
-    $(".page-node-61 .field-cta-subtitle .field-item").replaceWith("<div class=\"field-item\"></div>");
+     // Only global shamefull javascript. Per node modifications can be done with code-per-node module
   });
-  
 })(jQuery);
