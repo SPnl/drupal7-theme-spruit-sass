aadd58a99ab74306baf131b33cc7e33ee67e209e
Chris Eikelboom <ceikelboom@sp.nl>
2019-01-29 13:03:12 +0100

    Current live state

--- templates/webform-confirmation-68.tpl.php
+++ templates/webform-confirmation-68.tpl.php
@@ -61,12 +61,12 @@ header.content-header {
 			<div class="webform-confirmation">
 <h1>Bedankt voor je steun!</h1>
 <p>Wat fantastisch dat ook jij onze beweging steunt! Samen staan we op voor een rechtvaardige samenleving. We kiezen voor hoop. Hoop dat de rechtvaardigheid het wint van het ieder-voor-zich-denken. Door Vriend voor Rechtvaardigheid te worden heb jij een eerste stap gezet.</p>
-<p>Je ontvangt zo snel mogelijk jouw welkomstpakket. En er is meer: als vriend van rechtvaardigheid ben je van harte uitgenodigd om naar de speciale Meet & Greet met Lilian Marijnissen te komen!</p>
+<p>Je ontvangt zo snel mogelijk jouw welkomstpakket. En er is meer: als vriend van rechtvaardigheid ben je van harte uitgenodigd om naar de speciale vriendendag met Lilian Marijnissen te komen!</p>
 
 <?php
 
   $submissions = webform_get_submissions($node->nid);
-  $url = "/vrienden/meet-en-greet";
+  $url = "/rechtvaardigheid/vriendendag?ref=doemee_internal";
   $data = array();
   if($submissions != NULL) {
     if(!empty($submissions[$sid]->data[2][0])) {
@@ -75,16 +75,17 @@ header.content-header {
     if(!empty($submissions[$sid]->data[3][0])) {
       $data['tel'] = $submissions[$sid]->data[3][0];
     }
-    if(count($data) == 2) {
-      $url = $url . "?eml=" . $data['eml'] . "&tel=" .$data['tel'];
-    } elseif(count($data) == 1) {
-      foreach($data as $key => $value) {
-         $url = $url ."?". $key ."=" .$value;
-      }
+    if(!empty($submissions[$sid]->data[1][0])) {
+      $data['nm'] = $submissions[$sid]->data[1][0];
     }
+
+    foreach($data as $key => $value) {
+      $url = $url . "&" . $key . "=" . urlencode($value);
+    }
+
   }
 ?>
-<p><a href="<?php print($url); ?>" class="btn">Aanmelden voor <br/>Meet & greet</a></p>
+<p><a href="<?php print($url); ?>" class="btn">Aanmelden vriendendag</a></p>
 
 				    <h3>Of help meteen mee door meer mensen over onze beweging te vertellen:</h3>
 				    <ul class="share">
