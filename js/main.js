6a5f1f15350a60608c52c8d42eaf87089adf2331
Stijn Dekker <sdekker@sp.nl>
2019-09-06 12:25:08 +0200

    Fixes email field value duplication

--- js/main.js
+++ js/main.js
@@ -126,13 +126,13 @@
         }
     };
     Drupal.behaviors.trimEmailField = {
-        // Trim email on change
+        // Remove spaces in email on change
         // The jquery clientside validation plugin doesn't allow trailing spaces for email input and responds with 'invalid email'.
         // The user can be unaware of the space and unable to continue the form
         attach: function(context, settings) {
           $("input[type=email]").change(function(e){
-            var emailField = $("input[type=email]").val();
-            this.value = $.trim(emailField);
+            emailTrimmed = $(this).val().replace(/ /g,'');
+            $(this).val(emailTrimmed);
           });
         }
     };
