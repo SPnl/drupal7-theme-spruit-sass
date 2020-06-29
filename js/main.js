9e149c18c5dba2fcfa5bcd24fb92d610dc13476e
chriseikelboom <ceikelboom@sp.nl>
2020-06-29 15:30:00 +0200

    new accordion component

--- js/main.js
+++ js/main.js
@@ -191,5 +191,28 @@
         }
     };
     */
+    Drupal.behaviors.accordion = {
+        // Add a simple JS accordion (see w3)
+        attach: function(context, settings) {
+            var acc = document.getElementsByClassName("accordion");
+            var i;
+
+            for (i = 0; i < acc.length; i++) {
+              acc[i].addEventListener("click", function() {
+                /* Toggle between adding and removing the "active" class,
+                to highlight the button that controls the panel */
+                this.classList.toggle("active");
+
+                /* Toggle between hiding and showing the active panel */
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                  panel.style.display = "none";
+                } else {
+                  panel.style.display = "block";
+                }
+              });
+            }
+        }
+    };
 
 })(jQuery);
